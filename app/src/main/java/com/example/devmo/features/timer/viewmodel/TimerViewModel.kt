9f0e1c770cd881179983703b3aca79a1d443c4af
momo838 <morgane0044@sfr.fr>
2025-03-14 11:05:41 +0100

    page de résumé parce que les calendriers c'est trop dur

--- app/src/main/java/com/example/devmo/features/timer/viewmodel/TimerViewModel.kt
+++ app/src/main/java/com/example/devmo/features/timer/viewmodel/TimerViewModel.kt
@@ -7,12 +7,20 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+data class Revision(val startTime: String, val duration: Long)
 
 class TimerViewModel : ViewModel() {
     private val _timer = MutableStateFlow(0L)
     val timer = _timer.asStateFlow()
 
     private var timerJob: Job? = null
+
+    private val _revisions = MutableStateFlow<List<Revision>>(emptyList())
+    val revisions = _revisions.asStateFlow()
 
     fun startTimer() {
         timerJob?.cancel()
@@ -29,6 +37,14 @@ class TimerViewModel : ViewModel() {
     }
 
     fun stopTimer() {
+
+        val duration = _timer.value
+        val startTime = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(Date())
+        val revision = Revision(startTime, duration)
+
+        // Ajouter la révision à la liste
+        _revisions.value = _revisions.value + revision
+
         _timer.value = 0
         timerJob?.cancel()
     }
